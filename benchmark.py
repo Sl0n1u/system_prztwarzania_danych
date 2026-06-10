6783d3230651b914e0d72d08e1ef00191584da88
Bartek <bartekzbity@gmail.com>
2026-06-10 09:50:08 +0200

    Wersja ostateczna

--- benchmark.py
+++ benchmark.py
@@ -29,33 +29,37 @@ def run_benchmark():
         print(f"-> Testowanie zbioru danych: {dataset_name}")
         results[dataset_name] = {}
 
-        print("   [~] Trwa pomiar wersji sekwencyjnej...", end="", flush=True)
+        print("   [~] Trwa pomiar wersji sekwencyjnej... ", end="", flush=True)
         seq_times = []
-        for _ in range(REPETITIONS):
+        for i in range(REPETITIONS):
             with contextlib.redirect_stdout(io.StringIO()):
                 t = process_directory(dataset)
             seq_times.append(t)
+            # Drukujemy czas po kazdej iteracji w tej samej linii
+            print(f"[{i+1}: {t:.2f}s] ", end="", flush=True)
             
         avg_seq_time = sum(seq_times) / REPETITIONS
         results[dataset_name]['sequential'] = avg_seq_time
-        print(f" Gotowe (Srednia: {avg_seq_time:.2f}s)")
+        print(f"Gotowe (Srednia: {avg_seq_time:.2f}s)")
 
         results[dataset_name]['parallel'] = {}
         for workers in WORKER_COUNTS:
-            print(f"   [~] Trwa pomiar dla {workers} workerow...", end="", flush=True)
+            print(f"   [~] Trwa pomiar dla {workers} workerow... ", end="", flush=True)
             
             # Listy na poszczegolne metryki
             par_times = []
             map_times = []
             reduce_times = []
             
-            for _ in range(REPETITIONS):
+            for i in range(REPETITIONS):
                 with contextlib.redirect_stdout(io.StringIO()):
                     # Rozpakowanie 3 wartosci z koordynatora
                     t_total, t_map, t_reduce = run_parallel_system(dataset, num_workers=workers)
                 par_times.append(t_total)
                 map_times.append(t_map)
                 reduce_times.append(t_reduce)
+                # Drukujemy czas calkowity po kazdej iteracji w tej samej linii
+                print(f"[{i+1}: {t_total:.2f}s] ", end="", flush=True)
                 
             avg_par_time = sum(par_times) / REPETITIONS
             avg_map_time = sum(map_times) / REPETITIONS
@@ -71,7 +75,7 @@ def run_benchmark():
                 'speedup': speedup,
                 'efficiency': efficiency
             }
-            print(f" Gotowe (Srednia: {avg_par_time:.2f}s)")
+            print(f"Gotowe (Srednia: {avg_par_time:.2f}s)")
         print("-" * 50)
 
     print("\n\n" + "="*85)
